bb80f68e92b67831ae89a3ef8d2ae9c5dfdbfd79
Linards Kalvāns <linards.kalvans@gmail.com>
2019-11-24 00:34:24 +0200

    Fix typo

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-chmod 600 deploy_key
+chmod 600 deploy_rsa
   rsync -Pav -e "ssh -o StrictHostKeyChecking=no -i deploy_rsa" --exclude-from=./rsync-exclude.txt ./ts-viewver $DEPLOY_HOST:~/shiny/appdir/putnu31 --delete
